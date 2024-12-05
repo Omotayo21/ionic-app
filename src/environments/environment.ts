cbae252800bc91bdaef26186f7a48028114b3c8c
Rahman <rufaiabdulrahman21@gmail.com>
2024-12-05 01:31:28 +0100

    another change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,17 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+export interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
 export const environment = {
-
+  production: false,
   firebase: {
     apiKey: 'AIzaSyDAYORtT_yPLCmeN38hLh7ZhjOjc3N23yU',
     authDomain: 'citizen-report-app-45f6c.firebaseapp.com',
@@ -14,7 +22,7 @@ export const environment = {
     messagingSenderId: '382465207068',
     appId: '1:382465207068:web:2b647e65ba8807858c68d9',
     measurementId: 'G-9JFKSH4G0L',
-  },
+  } as FirebaseConfig
 };
 
 /*
