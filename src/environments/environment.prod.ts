8667b5cac7d473f23c19c84ae2d890c162634574
Rahman <rufaiabdulrahman21@gmail.com>
2024-12-05 01:39:50 +0100

    hopeful

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDAYORtT_yPLCmeN38hLh7ZhjOjc3N23yU',
+    authDomain: 'citizen-report-app-45f6c.firebaseapp.com',
+    projectId: 'citizen-report-app-45f6c',
+    databaseURL:
+      'https://citizen-report-app-45f6c-default-rtdb.firebaseio.com/',
+    storageBucket: 'citizen-report-app-45f6c.appspot.com',
+    messagingSenderId: '382465207068',
+    appId: '1:382465207068:web:2b647e65ba8807858c68d9',
+    measurementId: 'G-9JFKSH4G0L',
+  },
 };
